ed071e42da37563a866f1113e2ee1e682ee52e38
Dhaivat Pandya <dhaivatpandya@gmail.com>
2010-04-17 12:41:28 -0400

    Removed uneeded files

--- src/main.c
+++ src/main.c
@@ -3,8 +3,14 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
-int main (void) {
+/* Parse the configuration file */
+void parse_config(char *filename);
 
+int main (int argc, char **argv) {
+	printf("Starting LightServer...");
+	
 	return 0;
 }
